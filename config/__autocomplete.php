3c24c03ff0166c2d7dac7f243bfa0e1a5618635f
Pavel Klochkov <pv.klochkov@yandex.ru>
2023-09-08 23:47:39 +0300

    init project

---
+++ config/__autocomplete.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\web\Application;
+
+/**
+ * This class only exists here for IDE (PHPStorm/Netbeans/...) autocompletion.
+ * This file is never included anywhere.
+ * Adjust this file to match classes configured in your application config, to enable IDE autocompletion for custom components.
+ * ```php
+ * class __Rollbar {
+ * }
+ * ```
+ */
+class Yii {
+    /**
+     * @var Application|\yii\console\Application|__Application
+     */
+    public static $app;
+}
+
+/**
+ * @property yii\rbac\DbManager $authManager 
+ * @property \yii\web\User|__WebUser $user
+ * 
+ */
+class __Application {
+}
+
+/**
+ * @property app\modules\User\Model\User $identity
+ */
+class __WebUser {
+}
