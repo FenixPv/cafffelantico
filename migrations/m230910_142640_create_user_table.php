2daad508cdfc290e29d94eadd169e9f97f39091c
Pavel Klochkov <pv.klochkov@yandex.ru>
2023-09-10 18:17:32 +0300

    migrate create user table

---
+++ migrations/m230910_142640_create_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ * @noinspection PhpUnused
+ */
+class m230910_142640_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp(): void
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+            'login' => $this->string()->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string(),
+            'email' => $this->string()->notNull(),
+            'status' => $this->integer()->notNull()->defaultValue(200),
+        ]);
+
+        $this->createIndex('idx-user-login', '{{user}}', 'login', true);
+        $this->createIndex('idx-user-email', '{{user}}', 'email', true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown(): void
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
