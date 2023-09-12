87236dcbc41d11fe19e772a828765b1f52a03047
Pavel Klochkov <pv.klochkov@yandex.ru>
2023-09-12 14:36:44 +0300

    Авторизация пользователя рефакторинг

--- modules/cpanel/controllers/DefaultController.php
+++ modules/cpanel/controllers/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace app\modules\cpanel\controllers;
 
+use Yii;
 use yii\web\Controller;
+use yii\web\ForbiddenHttpException;
 
 /**
  * Default controller for the `cpanel` module
@@ -12,9 +14,13 @@ class DefaultController extends Controller
     /**
      * Renders the index view for the module
      * @return string
+     * @throws ForbiddenHttpException
      */
-    public function actionIndex()
+    public function actionIndex(): string
     {
+        if (!Yii::$app->user->can('allowCpanel')) {
+            $this->redirect('/login');
+        }
         return $this->render('index');
     }
 }
